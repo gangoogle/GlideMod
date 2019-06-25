cbbb813f7adefef0a1fb56d2adf6c452c7116f67
0125gang <0125gang@gmail.com>
2019-06-25 15:51:19 +0800

    修改关键帧

--- app/src/main/java/com/test/glidetest/MainActivity.java
+++ app/src/main/java/com/test/glidetest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.test.glidetest;
 
+import android.Manifest;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -7,24 +9,37 @@ import android.widget.ImageView;
 import com.gangoogle.glide.GlideMod;
 import com.gangoogle.glide.load.engine.BaseKey;
 import com.gangoogle.glide.load.engine.DiskCacheStrategy;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView ivImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView ivImage = findViewById(R.id.imageView);
-        GlideMod.with(this).load("http://pic25.nipic.com/20121205/10197997_003647426000_2.jpg", new BaseKey() {
-            @Override
-            public String getKey() {
-                return "KeyAs";
-            }
-        })
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .skipMemoryCache(true)
-                .onlyRetrieveFromCache(true)
-                .dontAnimate()
+        ivImage = findViewById(R.id.imageView);
+        new RxPermissions(this)
+                .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) throws Exception {
+                        loadImg();
+                    }
+                });
+    }
+
+    private void loadImg() {
+        final String videoPath = "/storage/emulated/0/DCIM/Camera/v0200f080000bgsjsrceae1efua6o8og.MP4";
+        GlideMod.with(this)
+                .load(videoPath, new BaseKey() {
+                    @Override
+                    public String getKey() {
+                        return videoPath+"s7s";
+                    }
+                })
                 .into(ivImage);
     }
 }
