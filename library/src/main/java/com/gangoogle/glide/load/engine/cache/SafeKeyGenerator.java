b3f6f9f355a7b9b0989d442d6847050a71109438
0125gang <0125gang@gmail.com>
2020-01-08 15:58:17 +0800

    MD5操作添加默认值

--- library/src/main/java/com/gangoogle/glide/load/engine/cache/SafeKeyGenerator.java
+++ library/src/main/java/com/gangoogle/glide/load/engine/cache/SafeKeyGenerator.java
@@ -2,6 +2,7 @@ package com.gangoogle.glide.load.engine.cache;
 
 import android.support.annotation.NonNull;
 import android.support.v4.util.Pools;
+import android.text.TextUtils;
 
 import com.gangoogle.glide.util.LruCache;
 import com.gangoogle.glide.util.Preconditions;
@@ -20,62 +21,67 @@ import java.security.NoSuchAlgorithmException;
 // Public API.
 @SuppressWarnings("WeakerAccess")
 public class SafeKeyGenerator {
-  private final LruCache<String, String> loadIdToSafeHash = new LruCache<>(1000);
-  private final Pools.Pool<PoolableDigestContainer> digestPool =
-      FactoryPools.threadSafe(
-          10,
-          new FactoryPools.Factory<PoolableDigestContainer>() {
-            @Override
-            public PoolableDigestContainer create() {
-              try {
-                return new PoolableDigestContainer(MessageDigest.getInstance("SHA-256"));
-              } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-              }
+    private final LruCache<String, String> loadIdToSafeHash = new LruCache<>(1000);
+    private final Pools.Pool<PoolableDigestContainer> digestPool =
+            FactoryPools.threadSafe(
+                    10,
+                    new FactoryPools.Factory<PoolableDigestContainer>() {
+                        @Override
+                        public PoolableDigestContainer create() {
+                            try {
+                                return new PoolableDigestContainer(MessageDigest.getInstance("SHA-256"));
+                            } catch (NoSuchAlgorithmException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    });
+
+
+    public synchronized String getSafeKey(String key) {
+        String safeKey;
+        synchronized (loadIdToSafeHash) {
+            safeKey = loadIdToSafeHash.get(key);
+        }
+        if (safeKey == null) {
+            if (TextUtils.isEmpty(key)) {
+                safeKey = Encode.md5Sync(System.currentTimeMillis() + "");
+            } else {
+                //改为md5
+                safeKey = Encode.md5Sync(key);
             }
-          });
-
-  public String getSafeKey(String key) {
-    String safeKey;
-    synchronized (loadIdToSafeHash) {
-      safeKey = loadIdToSafeHash.get(key.toString());
+        }
+        synchronized (loadIdToSafeHash) {
+            loadIdToSafeHash.put(key, safeKey);
+        }
+        return safeKey;
     }
-    if (safeKey == null) {
-      //改为md5
-      safeKey = Encode.md5(key);
-    }
-    synchronized (loadIdToSafeHash) {
-      loadIdToSafeHash.put(key, safeKey);
-    }
-    return safeKey;
-  }
 
 
-
-  private String calculateHexStringDigest(String key) {
-    PoolableDigestContainer container = Preconditions.checkNotNull(digestPool.acquire());
-    try {
+    private String calculateHexStringDigest(String key) {
+        PoolableDigestContainer container = Preconditions.checkNotNull(digestPool.acquire());
+        try {
 //      key.updateDiskCacheKey(container.messageDigest);
-      // calling digest() will automatically reset()
-      return Util.sha256BytesToHex(container.messageDigest.digest());
-    } finally {
-      digestPool.release(container);
-    }
-  }
-
-  private static final class PoolableDigestContainer implements FactoryPools.Poolable {
-
-    @Synthetic final MessageDigest messageDigest;
-    private final StateVerifier stateVerifier = StateVerifier.newInstance();
-
-    PoolableDigestContainer(MessageDigest messageDigest) {
-      this.messageDigest = messageDigest;
+            // calling digest() will automatically reset()
+            return Util.sha256BytesToHex(container.messageDigest.digest());
+        } finally {
+            digestPool.release(container);
+        }
     }
 
-    @NonNull
-    @Override
-    public StateVerifier getVerifier() {
-      return stateVerifier;
+    private static final class PoolableDigestContainer implements FactoryPools.Poolable {
+
+        @Synthetic
+        final MessageDigest messageDigest;
+        private final StateVerifier stateVerifier = StateVerifier.newInstance();
+
+        PoolableDigestContainer(MessageDigest messageDigest) {
+            this.messageDigest = messageDigest;
+        }
+
+        @NonNull
+        @Override
+        public StateVerifier getVerifier() {
+            return stateVerifier;
+        }
     }
-  }
 }
