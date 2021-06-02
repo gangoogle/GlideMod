3e56a43e432ed9a371b492516272f99f27d9f59b
gangoogle <gangoogle@gmail.com>
2021-06-02 16:31:43 +0800

    commit

--- library/src/main/java/com/gangoogle/glide/request/target/ImageViewTarget.java
+++ library/src/main/java/com/gangoogle/glide/request/target/ImageViewTarget.java
@@ -8,10 +8,12 @@ import android.graphics.drawable.Animatable;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.NinePatchDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Build;
 import android.widget.ImageView;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+
 import com.gangoogle.glide.request.transition.Transition;
 
 import java.io.ByteArrayOutputStream;
@@ -21,196 +23,213 @@ import java.io.ByteArrayOutputStream;
  * android.widget.ImageView}s.
  *
  * @param <Z> The type of resource that this target will display in the wrapped {@link
- *     android.widget.ImageView}.
+ *            android.widget.ImageView}.
  */
 // Public API.
 @SuppressWarnings("WeakerAccess")
 public abstract class ImageViewTarget<Z> extends ViewTarget<ImageView, Z>
-    implements Transition.ViewAdapter {
-  private final int BITMAP_MAX_SIZE = 100 * 1024 * 1024;
+        implements Transition.ViewAdapter {
+    private final int BITMAP_MAX_SIZE = 100 * 1024 * 1024;
 
-  @Nullable private Animatable animatable;
+    @Nullable
+    private Animatable animatable;
 
-  public ImageViewTarget(ImageView view) {
-    super(view);
-  }
-
-  /** @deprecated Use {@link #waitForLayout()} instead. */
-  @SuppressWarnings({"deprecation"})
-  @Deprecated
-  public ImageViewTarget(ImageView view, boolean waitForLayout) {
-    super(view, waitForLayout);
-  }
-
-  /**
-   * Returns the current {@link android.graphics.drawable.Drawable} being displayed in the view
-   * using {@link android.widget.ImageView#getDrawable()}.
-   */
-  @Override
-  @Nullable
-  public Drawable getCurrentDrawable() {
-    return view.getDrawable();
-  }
-
-  /**
-   * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
-   * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
-   *
-   * @param drawable {@inheritDoc}
-   */
-  @Override
-  public void setDrawable(Drawable drawable) {
-    if (drawable == null) {
-      view.setImageDrawable(drawable);
-      return;
+    public ImageViewTarget(ImageView view) {
+        super(view);
     }
-    Bitmap bitmap = drawable2Bitmap(drawable);
-    if (bitmap == null) {
-      view.setImageDrawable(drawable);
-      return;
+
+    /**
+     * @deprecated Use {@link #waitForLayout()} instead.
+     */
+    @SuppressWarnings({"deprecation"})
+    @Deprecated
+    public ImageViewTarget(ImageView view, boolean waitForLayout) {
+        super(view, waitForLayout);
     }
+
+    /**
+     * Returns the current {@link android.graphics.drawable.Drawable} being displayed in the view
+     * using {@link android.widget.ImageView#getDrawable()}.
+     */
+    @Override
+    @Nullable
+    public Drawable getCurrentDrawable() {
+        return view.getDrawable();
+    }
+
+    /**
+     * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
+     * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
+     *
+     * @param drawable {@inheritDoc}
+     */
+    @Override
+    public void setDrawable(Drawable drawable) {
+        if (drawable == null) {
+            view.setImageDrawable(drawable);
+            return;
+        }
+        if (drawable instanceof TransitionDrawable) {
+            TransitionDrawable transitionDrawable = (TransitionDrawable) drawable;
+            int drawableNum = transitionDrawable.getNumberOfLayers();
+            Drawable drawableList[] = new Drawable[drawableNum];
+            for (int i = 0; i < drawableNum; i++) {
+                Drawable inDrawAble = transitionDrawable.getDrawable(i);
+                Bitmap bitmap = drawable2Bitmap(inDrawAble);
+                bitmap = compressBitmap(bitmap);
+                drawableList[i]=new BitmapDrawable(bitmap);
+            }
+            TransitionDrawable newTrans =  new TransitionDrawable(drawableList);
+            view.setImageDrawable(newTrans);
+            return;
+        }
+        Bitmap bitmap = drawable2Bitmap(drawable);
+        if (bitmap == null) {
+            view.setImageDrawable(drawable);
+            return;
+        }
 //        Log.d("bitmap", "ordinal:" + bitmap.getConfig().ordinal() + "-size:" + getBitmapSize(bitmap));
-    bitmap = compressBitmap(bitmap);
+        bitmap = compressBitmap(bitmap);
 //        Log.d("bitmap", "new -ordinal:" + bitmap.getConfig().ordinal() + "-size:" + getBitmapSize(bitmap));
-    view.setImageDrawable(new BitmapDrawable(bitmap));
-  }
-
-  /**
-   * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
-   * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
-   *
-   * @param placeholder {@inheritDoc}
-   */
-  @Override
-  public void onLoadStarted(@Nullable Drawable placeholder) {
-    super.onLoadStarted(placeholder);
-    setResourceInternal(null);
-    setDrawable(placeholder);
-  }
-
-  /**
-   * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
-   * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
-   *
-   * @param errorDrawable {@inheritDoc}
-   */
-  @Override
-  public void onLoadFailed(@Nullable Drawable errorDrawable) {
-    super.onLoadFailed(errorDrawable);
-    setResourceInternal(null);
-    setDrawable(errorDrawable);
-  }
-
-  /**
-   * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
-   * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
-   *
-   * @param placeholder {@inheritDoc}
-   */
-  @Override
-  public void onLoadCleared(@Nullable Drawable placeholder) {
-    super.onLoadCleared(placeholder);
-    if (animatable != null) {
-      animatable.stop();
+        view.setImageDrawable(new BitmapDrawable(bitmap));
     }
-    setResourceInternal(null);
-    setDrawable(placeholder);
-  }
 
-  @Override
-  public void onResourceReady(@NonNull Z resource, @Nullable Transition<? super Z> transition) {
-    if (transition == null || !transition.transition(resource, this)) {
-      setResourceInternal(resource);
-    } else {
-      maybeUpdateAnimatable(resource);
+    /**
+     * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
+     * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
+     *
+     * @param placeholder {@inheritDoc}
+     */
+    @Override
+    public void onLoadStarted(@Nullable Drawable placeholder) {
+        super.onLoadStarted(placeholder);
+        setResourceInternal(null);
+        setDrawable(placeholder);
     }
-  }
 
-  @Override
-  public void onStart() {
-    if (animatable != null) {
-      animatable.start();
+    /**
+     * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
+     * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
+     *
+     * @param errorDrawable {@inheritDoc}
+     */
+    @Override
+    public void onLoadFailed(@Nullable Drawable errorDrawable) {
+        super.onLoadFailed(errorDrawable);
+        setResourceInternal(null);
+        setDrawable(errorDrawable);
     }
-  }
 
-  @Override
-  public void onStop() {
-    if (animatable != null) {
-      animatable.stop();
+    /**
+     * Sets the given {@link android.graphics.drawable.Drawable} on the view using {@link
+     * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable)}.
+     *
+     * @param placeholder {@inheritDoc}
+     */
+    @Override
+    public void onLoadCleared(@Nullable Drawable placeholder) {
+        super.onLoadCleared(placeholder);
+        if (animatable != null) {
+            animatable.stop();
+        }
+        setResourceInternal(null);
+        setDrawable(placeholder);
     }
-  }
 
-  private void setResourceInternal(@Nullable Z resource) {
-    // Order matters here. Set the resource first to make sure that the Drawable has a valid and
-    // non-null Callback before starting it.
-    setResource(resource);
-    maybeUpdateAnimatable(resource);
-  }
-
-  private void maybeUpdateAnimatable(@Nullable Z resource) {
-    if (resource instanceof Animatable) {
-      animatable = (Animatable) resource;
-      animatable.start();
-    } else {
-      animatable = null;
+    @Override
+    public void onResourceReady(@NonNull Z resource, @Nullable Transition<? super Z> transition) {
+        if (transition == null || !transition.transition(resource, this)) {
+            setResourceInternal(resource);
+        } else {
+            maybeUpdateAnimatable(resource);
+        }
     }
-  }
 
-  protected abstract void setResource(@Nullable Z resource);
+    @Override
+    public void onStart() {
+        if (animatable != null) {
+            animatable.start();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (animatable != null) {
+            animatable.stop();
+        }
+    }
+
+    private void setResourceInternal(@Nullable Z resource) {
+        // Order matters here. Set the resource first to make sure that the Drawable has a valid and
+        // non-null Callback before starting it.
+        setResource(resource);
+        maybeUpdateAnimatable(resource);
+    }
+
+    private void maybeUpdateAnimatable(@Nullable Z resource) {
+        if (resource instanceof Animatable) {
+            animatable = (Animatable) resource;
+            animatable.start();
+        } else {
+            animatable = null;
+        }
+    }
+
+    protected abstract void setResource(@Nullable Z resource);
 
 
-  private Bitmap compressBitmap(Bitmap bitmap) {
-    if (getBitmapSize(bitmap) < BITMAP_MAX_SIZE) {
-      return bitmap;
+    private Bitmap compressBitmap(Bitmap bitmap) {
+        if (getBitmapSize(bitmap) < BITMAP_MAX_SIZE) {
+            return bitmap;
+        }
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inSampleSize = 1;
+        opts.inPreferredConfig = Bitmap.Config.RGB_565;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int quality = 100;
+        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos);
+        // 循环判断压缩后图片是否超过限制大小
+        while (baos.toByteArray().length > BITMAP_MAX_SIZE) {
+            // 清空baos
+            baos.reset();
+            quality -= 10;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos);
+        }
+        bitmap = BitmapFactory.decodeByteArray(baos.toByteArray(), 0, baos.size(), opts);
+        return bitmap;
     }
-    BitmapFactory.Options opts = new BitmapFactory.Options();
-    opts.inSampleSize = 1;
-    opts.inPreferredConfig = Bitmap.Config.RGB_565;
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    int quality = 100;
-    bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos);
-    // 循环判断压缩后图片是否超过限制大小
-    while (baos.toByteArray().length > BITMAP_MAX_SIZE) {
-      // 清空baos
-      baos.reset();
-      quality -= 10;
-      bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos);
-    }
-    bitmap = BitmapFactory.decodeByteArray(baos.toByteArray(), 0, baos.size(), opts);
-    return bitmap;
-  }
 
-  /**
-   * 得到bitmap的大小
-   */
-  public static int getBitmapSize(Bitmap bitmap) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {    //API 19
-      return bitmap.getAllocationByteCount();
+    /**
+     * 得到bitmap的大小
+     */
+    public static int getBitmapSize(Bitmap bitmap) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {    //API 19
+            return bitmap.getAllocationByteCount();
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {//API 12
+            return bitmap.getByteCount();
+        }
+        // 在低版本中用一行的字节x高度
+        return ((bitmap.getRowBytes() * bitmap.getHeight()));                //earlier version
     }
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {//API 12
-      return bitmap.getByteCount();
-    }
-    // 在低版本中用一行的字节x高度
-    return ((bitmap.getRowBytes() * bitmap.getHeight()));                //earlier version
-  }
 
-  public static Bitmap drawable2Bitmap(Drawable drawable) {
-    if (drawable instanceof BitmapDrawable) {
-      return ((BitmapDrawable) drawable).getBitmap();
-    } else if (drawable instanceof NinePatchDrawable) {
-      Bitmap bitmap = Bitmap
-              .createBitmap(
-                      drawable.getIntrinsicWidth(),
-                      drawable.getIntrinsicHeight(),
-                      drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
-                              : Bitmap.Config.RGB_565);
-      Canvas canvas = new Canvas(bitmap);
-      drawable.setBounds(0, 0, drawable.getIntrinsicWidth(),
-              drawable.getIntrinsicHeight());
-      drawable.draw(canvas);
-      return bitmap;
-    } else {
-      return null;
+    public static Bitmap drawable2Bitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        } else if (drawable instanceof NinePatchDrawable) {
+            Bitmap bitmap = Bitmap
+                    .createBitmap(
+                            drawable.getIntrinsicWidth(),
+                            drawable.getIntrinsicHeight(),
+                            drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+                                    : Bitmap.Config.RGB_565);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, drawable.getIntrinsicWidth(),
+                    drawable.getIntrinsicHeight());
+            drawable.draw(canvas);
+            return bitmap;
+        } else {
+            return null;
+        }
     }
-  }
 }
